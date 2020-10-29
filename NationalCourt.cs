598e54e1542e1fdec5d6d7f47b84d5a07eb14696
yanchev93 <67742222+yanchev93@users.noreply.github.com>
2020-10-29 23:03:58 +0200

    Add files via upload

---
+++ NationalCourt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MidExam_29._02._2020_G2
+{
+    class NationalCourt
+    {
+        static void Main()
+        {
+            int employeOne = int.Parse(Console.ReadLine());
+            int employeTwo = int.Parse(Console.ReadLine());
+            int employeThree = int.Parse(Console.ReadLine());
+            int totalPeople = int.Parse(Console.ReadLine());
+
+            int answerQuestionsPerHour = employeOne + employeTwo + employeThree;
+            int hours = 0;
+            while (totalPeople > 0)
+            {
+                hours += 1;
+
+                if (hours % 4 == 0)
+                {
+                    continue;
+                }
+
+                totalPeople -= answerQuestionsPerHour;
+            }
+
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
